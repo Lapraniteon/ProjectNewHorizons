cd80872d89fd19a86ca6183de503b0a972cc45b6
Lapraniteon <lapraniteon@gmail.com>
2025-06-17 16:25:59 +0200

    Fine-tune grapple mechanic

--- Assets/Scripts/Player/Grapple.cs
+++ Assets/Scripts/Player/Grapple.cs
@@ -7,17 +7,29 @@ public class Grapple : MonoBehaviour
     public Vector2 targetJointTarget;
     public bool isGrappled = false;
     public bool isStationary = false;
+
+    public Rigidbody2D body;
+    public Vector2 forceTarget;
+    public float forceMultiplier;
+    public bool isGrappling;
     
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 raycastTarget = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+            /*if (isGrappling)
+            {
+                isGrappling = false;
+                targetJoint.enabled = false;
+                return;
+            }*/
+            
+            Vector3 raycastTarget = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z - Camera.main.transform.position.z));
             
             Debug.Log(raycastTarget);
 
             Vector2 direction = raycastTarget - transform.position;
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, Mathf.Infinity, LayerMask.GetMask("Grapple"));
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 11f, LayerMask.GetMask("Grapple"));
             
             if (hit)
             {
@@ -26,7 +38,24 @@ public class Grapple : MonoBehaviour
 
                 targetJoint.enabled = true;
                 targetJoint.target = hit.point;
+                /*isGrappling = true;*/
+                /*forceTarget = hit.point;
+                isGrappling = true;*/
             }
         }
+
+        /*if (Vector2.Distance(transform.position, forceTarget) < 1f && isGrappling)
+        {
+            isGrappling = false;
+            body.linearVelocity /= 2f;
+        }*/
     }
+
+    /*void FixedUpdate()
+    {
+        if (isGrappling)
+        {
+            body.AddForce(forceTarget * forceMultiplier);
+        }
+    }*/
 }
