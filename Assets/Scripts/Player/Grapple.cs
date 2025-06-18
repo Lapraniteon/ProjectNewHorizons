755487a75e086e315f354f2a55d163b9513f3939
Lapraniteon <lapraniteon@gmail.com>
2025-06-18 10:58:15 +0200

    Touch Controls

--- Assets/Scripts/Player/Grapple.cs
+++ Assets/Scripts/Player/Grapple.cs
@@ -3,59 +3,79 @@ using UnityEngine;
 public class Grapple : MonoBehaviour
 {
 
-    public TargetJoint2D targetJoint;
-    public Vector2 targetJointTarget;
-    public bool isGrappled = false;
-    public bool isStationary = false;
+    private Camera _mainCamera;
 
-    public Rigidbody2D body;
-    public Vector2 forceTarget;
-    public float forceMultiplier;
-    public bool isGrappling;
+    public TargetJoint2D targetJoint;
+    private Vector2 _touchStartPos;
+    private Vector2 _touchStartPosWorldSpace;
+    private Vector2 _direction;
+    private Vector2 _directionWorldSpace;
+    private bool _directionChosen;
+
+    void Start()
+    {
+        _mainCamera = Camera.main;
+    }
     
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        
+
+        float cameraZOffset = transform.position.z - Camera.main.transform.position.z;
+        
+        // Track a single touch as a direction control.
+        if (Input.touchCount > 0)
         {
-            /*if (isGrappling)
-            {
-                isGrappling = false;
-                targetJoint.enabled = false;
-                return;
-            }*/
-            
-            Vector3 raycastTarget = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z - Camera.main.transform.position.z));
-            
-            Debug.Log(raycastTarget);
+            Touch touch = Input.GetTouch(0);
 
-            Vector2 direction = raycastTarget - transform.position;
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 11f, LayerMask.GetMask("Grapple"));
-            
-            if (hit)
+            // Handle finger movements based on touch phase.
+            switch (touch.phase)
             {
-                Debug.DrawRay(transform.position, direction, Color.red, 2f);
-                DebugExtension.DebugPoint(hit.point, Color.cyan, 1f, 2f);
+                // Record initial touch position.
+                case TouchPhase.Began:
+                    _touchStartPos = touch.position;
+                    _touchStartPosWorldSpace = _mainCamera.ScreenToWorldPoint(new Vector3(_touchStartPos.x, _touchStartPos.y, cameraZOffset));
+                    _directionChosen = false;
+                    break;
 
-                targetJoint.enabled = true;
-                targetJoint.target = hit.point;
-                /*isGrappling = true;*/
-                /*forceTarget = hit.point;
-                isGrappling = true;*/
+                // Determine direction by comparing the current touch position with the initial one.
+                case TouchPhase.Moved:
+                    _directionWorldSpace = (Vector2) _mainCamera.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, cameraZOffset)) - _touchStartPosWorldSpace;
+                    break;
+
+                // Report that a direction has been chosen when the finger is lifted.
+                case TouchPhase.Ended:
+                    _directionChosen = true;
+                    break;
             }
         }
-
-        /*if (Vector2.Distance(transform.position, forceTarget) < 1f && isGrappling)
+        if (_directionChosen)
         {
-            isGrappling = false;
-            body.linearVelocity /= 2f;
-        }*/
+            Debug.Log("Launch!");
+            RaycastAndMove(_directionWorldSpace);
+            _directionChosen = false;
+        }
+
+        DebugExtension.DebugPoint(_touchStartPosWorldSpace, Color.cyan);
+        Debug.DrawRay(_touchStartPosWorldSpace, _directionWorldSpace, _directionChosen ? Color.green : Color.red);
     }
 
-    /*void FixedUpdate()
+    private void RaycastAndMove(Vector2 direction)
     {
-        if (isGrappling)
+        /*Vector3 raycastTarget = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z - Camera.main.transform.position.z));
+
+        Debug.Log(raycastTarget);
+
+        Vector2 direction = raycastTarget - transform.position;*/
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 11f, LayerMask.GetMask("Grapple"));
+
+        if (hit)
         {
-            body.AddForce(forceTarget * forceMultiplier);
+            Debug.DrawRay(transform.position, direction, Color.red, 2f);
+            DebugExtension.DebugPoint(hit.point, Color.cyan, 1f, 2f);
+
+            targetJoint.enabled = true;
+            targetJoint.target = hit.point;
         }
-    }*/
+    }
 }
