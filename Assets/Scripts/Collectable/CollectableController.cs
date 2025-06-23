dace13f870ebdc28b0b51d2a82a6bf1a20b69569
Lapraniteon <lapraniteon@gmail.com>
2025-06-23 14:09:47 +0200

    Implement Collectable scripts

---
+++ Assets/Scripts/Collectable/CollectableController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CollectableController : MonoBehaviour
+{
+
+    public Collectable[] collectables;
+    
+    public int Amount { get { return collectables.Length; } }
+    public int AmountCollected { get; private set; }
+    
+    public bool AllCollected { get { return AmountCollected == Amount; } }
+    
+    void Start()
+    {
+        GameManager.Instance.currentCollectableController = this;
+    }
+
+    public void GotCollectable(Collectable collectable)
+    {
+        AmountCollected++;
+    }
+}
