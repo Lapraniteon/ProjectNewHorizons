2739cdb5e646f682f9f68a7b0e6f3aa55d51e6c9
Lapraniteon <lapraniteon@gmail.com>
2025-06-24 15:52:32 +0200

    Add Unity Events to everything

--- Assets/Scripts/Collectable/Collectable.cs
+++ Assets/Scripts/Collectable/Collectable.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Collectable : MonoBehaviour
 {
     [HideInInspector] public CollectableController controller;
+
+    public UnityEvent onCollect;
     
     public bool IsCollected { get; private set; }
 
@@ -11,5 +14,9 @@ public class Collectable : MonoBehaviour
         controller.GotCollectable(this);
         gameObject.SetActive(false);
         IsCollected = true;
+        
+        onCollect.Invoke();
     }
+    
+    
 }
